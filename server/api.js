141c23f34e84ba9099c5d72679413b50f9d92acb
Raghupathy-vs <raghupathym7@outlook.com>
2025-07-25 13:34:20 +0530

    Modify one file name : api.js

--- server/api.js
+++ server/api.js
@@ -2,16 +2,13 @@
 const express = require('express');
 const router = express.Router();
 
-// Exported 'rooms' Map will be injected later from server.js
 let roomsRef = null;
 
-// Middleware to inject the rooms Map
 router.use((req, res, next) => {
   if (!roomsRef) return res.status(500).json({ error: "Room data not available" });
   next();
 });
 
-// GET /api/rooms - List all live rooms with users
 router.get('/rooms', (req, res) => {
   const roomList = [];                      // created to store details of all active rooms.
 
@@ -26,7 +23,6 @@ router.get('/rooms', (req, res) => {
   res.json({ activeRooms: roomList });
 });
 
-// Allow server.js to inject 'rooms'
 module.exports = {
   router,
   setRoomsMap: (rooms) => roomsRef = rooms
